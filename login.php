a7c675aa5142613a377dc39ab3176aac32bec949
Bram Cooper <bram.copermans180401@gmail.com>
2021-08-24 14:50:55 +0200

    basic setup + added readme, indexpage, loginpage, registerpage. feat: sessions in login page.

---
+++ login.php
@@ -0,0 +1,61 @@
+<?php
+function canLogin($email, $password)
+{
+    if ($email === "j@a" && $password === "12345") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if (!empty($_POST)) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if (canLogin($email, $password)) {
+        session_start();
+        $_SESSION["email"] = $email;
+    } else {
+        $error = true;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./styles/style.css">
+    <title>Login Page</title>
+</head>
+
+<body>
+    <img class="logoImg" src="./img/BorrowBoxFinalLogo.png" alt="BorrowBoxLogo">
+
+    <div class="d-flex justify-content-center mt-3">
+        <h3 class="text-uppercase">Log in</h3>
+    </div>
+
+    <div class="row justify-content-center align-items-center">
+        <form action="" method="post" class="loginForm">
+            <div class="col d-flex justify-content-center m-2"><input class="form-control" type="email" placeholder="email" name="email"></div>
+            <div class="col d-flex justify-content-center m-2"><input class="form-control" type="password" placeholder="password" name="password"></div>
+            <div class="col d-flex justify-content-center m-3"><input class="btn btnCustom" type="submit" name="login" value="login"></div>
+        </form>
+    </div>
+
+    <div class="error">
+        <p style="color:red; text-align:center">Incorrect email or password </p>
+    </div>
+
+
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,39 @@
+<?php
+var_dump($_POST);
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./styles/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <img class="logoImg" src="./img/BorrowBoxFinalLogo.png" alt="BorrowBoxLogo">
+
+    <a class="d-flex justify-content-center btn btnCustom m-5" href="login.php">log in with existing account</a>
+    <h3 class="display-6 d-flex justify-content-center">or make a new account</h3>
+
+    <form class="col justify-content-center" action="" method="post" enctype="multipart/form-data">
+        <div class="col d-flex justify-content-center m-3"><input class="form-control" type="text" name="firstname" placeholder="firstname"></div>
+        <div class="col d-flex justify-content-center m-3"><input class="form-control" type="text" name="lastname" placeholder="lastname"></div>
+        <div class="col d-flex justify-content-center m-3"><input class="form-control" type="email" name="email" placeholder="email"></div>
+        <div class="col d-flex justify-content-center m-3"><input class="form-control" type="password" name="password" placeholder="password"></div>
+        <div class="col d-flex justify-content-center m-3"><input type="submit" class="btn btnCustom" value="create account"></div>
+    </form>
+
+    <?php if (isset($error)) : ?>
+        <div class="error"><?php echo $error ?></div>
+    <?php endif; ?>
+
+</body>
+
+</html>
